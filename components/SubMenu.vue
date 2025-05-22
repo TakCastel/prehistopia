e6ad174fd6c98b6144c77c2e69d4f483581d5d98
Tak Castel <takcastel@gmail.com>
2025-05-22 19:27:43 +0200

    fix menu scroll

--- components/SubMenu.vue
+++ components/SubMenu.vue
@@ -1,18 +1,23 @@
 <template>
   <div
-    class="fixed top-[44px] right-16 h-full w-48 bg-[#3B2A21] text-white p-2 z-50 border-l border-[#2E211B] flex flex-col gap-2 shadow-lg"
+    class="fixed top-[44px] right-16 h-full max-h-[calc(100vh-44px)] w-48 bg-[#3B2A21] text-white p-2 z-50 border-l border-[#2E211B] flex flex-col shadow-lg"
   >
     <div class="font-bold text-[#EEDFC6] mb-1">
       {{ category.name }}
     </div>
 
-    <BuildingButton
-      v-for="building in category.buildings"
-      :key="building.name"
-      :building="building"
-      :building-config="buildingConfig"
-      @click="() => handleSelect(building)"
-    />
+    <!-- Zone scrollable ici -->
+    <div
+      class="flex-1 overflow-y-auto flex flex-col gap-2 touch-auto overscroll-contain"
+    >
+      <BuildingButton
+        v-for="building in category.buildings"
+        :key="building.name"
+        :building="building"
+        :building-config="buildingConfig"
+        @click="() => handleSelect(building)"
+      />
+    </div>
   </div>
 </template>
 
